caf0f6a841fd48d931c76fe93eee6b042e8bae9a
s1ash-cmd <58273072+s1ash-cmd@users.noreply.github.com>
2023-07-21 15:00:29 +0300

    Add files via upload

---
+++ petrov_as4/app.js
@@ -0,0 +1,13 @@
+﻿window.addEventListener('load', async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const reg = await navigator.serviceWorker.register(/serviseworker.js)
+            console.log('da', reg)
+        }
+        catch (e) {
+            colsole.log('net')
+        }
+
+                                            
+    }
+})
